efca43abe216c1b6c4bb6b2832dd2393f0f780af
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-09-02 09:41:45 +0000

    Verbose messages for browsing features
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@6245 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/__init__.py
+++ recensio/policy/__init__.py
@@ -23,19 +23,26 @@ def initialize(context):
 def reloadProfiles(br):
     profiles = ['profile-recensio.policy:default'] + additional_profiles
     for profile in profiles:
+        print "Trying to load profile %s" % profile
         br.open(host + '/portal_setup/manage_importSteps')
         br.getControl(name='context_id', index=0).value = [profile]
         br.getForm('profileform').submit()
         br.getControl('Import all steps').click()
+        print "Profile %s loaded" % profile
 
 def resetCatalog(br):
+    print "Trying to reindex catalog"
     br.open(host + '/portal_catalog/manage_catalogAdvanced')
     br.getControl('Clear and Rebuild').click()
     assert 'Catalog Rebuilt' in br.contents
+    print "Catalog reindexed"
+    print "Trying to clear solr"
     br.open(host + '/@@solr-maintenance/clear')
     assert 'solr index cleared.' in br.contents
+    print "Trying to reindex solr"
     br.open(host + '/@@solr-maintenance/reindex')
     assert 'solr index rebuilt.' in br.contents
+    print "All solr tasks done"
 
 def reset():
     br = Browser(sys.argv[1])
@@ -48,9 +55,11 @@ def reset():
 def createSite():
     base64string = base64.encodestring('%s:%s' % (user, passwd))[:-1]
     br = Browser(sys.argv[1])
+    print "Trying to create new plone site"
     br.addHeader('Authorization', 'Basic %s' % base64string)
     br.getControl('Create a new Plone site').click()
     br.getControl(name = 'site_id').value = 'recensio'
     br.getControl(name = 'extension_ids:list', index=2).value = ['recensio.policy:default']
     br.getControl('Create Plone Site').click()
+    print "Plone site created"
 
