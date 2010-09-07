6a2a9e7bc1cca9f1072097b5bb37e4b33129c6dc
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-09-07 17:00:22 +0000

    Dynamic views for topics in the footer
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@6349 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/browser/footer_viewlet.py
+++ recensio/policy/browser/footer_viewlet.py
@@ -3,6 +3,10 @@ import random
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewlet
 
+from Products.CMFCore.utils import getToolByName
+
+from recensio.policy.utility import getSelectedQuery
+
 from plone.app.layout.viewlets import ViewletBase
 
 class Footer(ViewletBase):
@@ -19,3 +23,18 @@ class Footer(ViewletBase):
             return pubs
         else:
             return []
+
+    @property
+    def drill_down_views(self):
+        vocabulary = getToolByName(self.context, 'portal_vocabularies')
+        if not hasattr(self.context, 'themen-epochen-regionen'):
+            return []
+        getUrl = lambda x: '?'.join([\
+            getattr(self.context, 'themen-epochen-regionen').absolute_url(), \
+            getSelectedQuery({'ddcTime' : [vocabulary['epoch_values'][x].getTermKey()]})])
+        views = [{'title' : 'Alte Geschichte', 'url' : getUrl('old history')}
+                ,{'title' : 'Mittelalter', 'url' : getUrl('middle_age')}
+                ,{'title' : 'Neuzeit bis 1900', 'url' : getUrl('modern_age_until_1900')}
+                ,{'title' : '20. Jahrhundert', 'url' : getUrl('20_century')}
+                ,{'title' : '21. Jahrhundert', 'url' : getUrl('21_century')}]
+        return views
