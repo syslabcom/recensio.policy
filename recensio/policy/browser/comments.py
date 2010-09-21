d2af2d49dffd52adc529969ca65d1671511fc1ca
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-09-21 11:43:31 +0000

    Replacing the comment template
    Now the template contains mailto links that allow somebody
    to send a message if some comment might be bad
    this fixes #1713
    
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@6451 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/policy/browser/comments.py
@@ -0,0 +1,29 @@
+from urllib import quote_plus
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+from plone.app.controlpanel.mail import IMailSchema
+
+from plone.app.discussion.browser.comments import CommentsViewlet as BaseCommentsViewlet
+
+class CommentsViewlet(BaseCommentsViewlet):
+    index = ViewPageTemplateFile('templates/comments.pt')
+
+    @property
+    def getMailTarget(self):
+        if not hasattr(self, '_mailTarget'):
+            root = getToolByName(self.context, 'portal_url').getPortalObject()
+            schema = IMailSchema(root)
+            self._mailTarget = '%s <%s>' % (schema.email_from_name, schema.email_from_address)
+        return self._mailTarget
+
+    @property
+    def title(self):
+        if not hasattr(self, '_mailTitle'):
+            self._mailTitle = 'NOTIFICATION: possible problematic comment at %s#' % self.context.absolute_url()
+        return self._mailTitle
+
+    @property
+    def escaped_title(self):
+        return quote_plus(self.title)
