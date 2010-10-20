06872f14e5a2f860c76cf575aa579ef4e60344b9
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-10-20 12:27:19 +0000

    Forgot to add files
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@6762 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/policy/profiles/default/workflows/simple_publication_workflow/scripts/handle_change.py
@@ -0,0 +1,11 @@
+## Script (Python) "handle_change"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=info
+##title=
+##
+rwh = context.restrictedTraverse('@@recensio_workflow_helper')
+rwh.handleTransition(info)
