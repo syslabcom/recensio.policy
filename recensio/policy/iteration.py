25c0304b71528903fba21fc7ed685fdbb81b5d2f
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-10-20 11:41:51 +0000

    Changing the plone.app.iterate functionality
    it will now successfully delete objects even though
    we prohibitid deleting. doesnt make sense to keep working
    copies of "deleted" documents.
    Also take care that after successful deletion, the user
    does not get redirected to the deleted document, since he would get
    an unauthorized too
    
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@6755 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/policy/iteration.py
@@ -0,0 +1,18 @@
+from AccessControl import getSecurityManager
+from Acquisition import aq_parent, aq_inner
+from OFS.Folder import Folder
+from plone.app.iterate.policy import CheckinCheckoutPolicyAdapter
+from plone.app.iterate import event
+from Products.CMFCore import permissions
+from zope.event import notify
+
+class CustomCheckingCheckoutPolicyAdapter(CheckinCheckoutPolicyAdapter):
+    def cancelCheckout(self):
+        baseline = self._getBaseline()
+        notify(event.CancelCheckoutEvent(self.context, baseline))
+        wc_container = aq_parent(aq_inner(self.context))
+        Folder.manage_delObjects(wc_container, [self.context.getId()])
+        if getSecurityManager().checkPermission(permissions.View, baseline):
+            return baseline
+        else:
+            return aq_parent(baseline)
