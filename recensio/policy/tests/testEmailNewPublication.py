cd014131b57bd99417d9b66cff68f0b3c60a6cd5
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-11-11 16:32:34 +0000

    added context_url to notification mail text
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@6900 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/tests/testEmailNewPublication.py
+++ recensio/policy/tests/testEmailNewPublication.py
@@ -30,12 +30,14 @@ Die folgende Mail wurde generiert zur Benachrichtigung von Tadeusz Kot\u0142owsk
 
 **********
 
-Dear Mr Tadeusz Kot\u0142owski,
+Dear Mr Fran\xc3\xa7ois Lam\xc3\xa8re,
 
 Recently a text on the topic Test PresentationArticleReview No 0 Dzieje pa\u0144stwa i spo\u0142ecze\u0144stwa 1890\u20131945 was published.
 Its author \u0425\u0440\u0438\u0441\u0442\u043e \u0421\u0442\u043e\u0438\u0447\u043a\u043e\u0432 has presented the text on the review platform recensio.net and states that he/she has dealt with your research.
 
 Here you can take a look at the presentation and comment on the presented statements. You will only need to register first (fast and free of charge) with your name and e-mail-address. This is necessary to prevent any misuse of the comment function and to maintain the platform’s scholarly claim.
+
+http://nohost/plone/...
 
 For any further information/questions please feel free to contact the recensio.net editorial team:
  <fake>.
@@ -44,11 +46,13 @@ Yours sincerely
 The recensio.net editorial team
 
 recensio.net is a joint project of the Bavarian State Library (BSB) Munich, the German Historical Institute Paris (DHIP) and the Institute for European History (IEG) Mainz – funded by the German Research Foundation (DFG). Further information http://nohost/plone/ueberuns/konzept'''
-        expected_mail_de = u'''Sehr geehrter Herr Tadeusz Kot\u0142owski,
+        expected_mail_de = u'''Sehr geehrter Herr Fran\xc3\xa7ois Lam\xc3\xa8re,
 
 vor Kurzem ist eine Schrift zum Thema Test PresentationArticleReview No 0 Dzieje pa\u0144stwa i spo\u0142ecze\u0144stwa 1890\u20131945 erschienen. Der Autor \u0425\u0440\u0438\u0441\u0442\u043e \u0421\u0442\u043e\u0438\u0447\u043a\u043e\u0432 hat diese Schrift auf der Rezensionsplattform recensio.net pr\xe4sentiert und gibt an, sich mit Ihren Forschungen auseinandergesetzt zu haben.
 
 Sie k\xf6nnen die Pr\xe4sentation hier einsehen und haben zugleich die Gelegenheit, die pr\xe4sentierten Thesen zu kommentieren. Daf\xfcr ist eine kurze, kostenlose Registrierung mit Namen und E-Mail-Adresse notwendig, die lediglich dazu dient, Missbrauch der Kommentarfunktion zu verhindern und den wissenschaftlichen Anspruch der Plattform zu wahren.
+
+http://nohost/plone/...
 
 F\xfcr R\xfcckfragen steht Ihnen die recensio.net-Redaktion gern zur Verf\xfcgung:  <fake>.
 
