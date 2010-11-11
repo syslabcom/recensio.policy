0cfebfce0b459787e3ac44e488a0e1f2a3f5e980
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-11-11 16:05:36 +0000

    Small helper to fix issue with #2200
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@6897 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/policy/browser/fixer.py
@@ -0,0 +1,7 @@
+from Products.Five.browser import BrowserView
+import urllib
+
+class Fixer(BrowserView):
+    def __call__(self):
+        self.context.setCoverPicture(None)
+        return "Deleted Cover image"
