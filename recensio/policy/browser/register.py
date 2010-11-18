234acf24552ce9e4a6605667d4fc0c89ec3974cb
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-11-18 12:00:21 +0000

    neat little extension of RegistrationForm that sets fullname if not specified
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@7006 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/policy/browser/register.py
@@ -0,0 +1,15 @@
+from plone.app.users.browser.register import RegistrationForm
+
+class RecensioRegistrationForm(RegistrationForm):
+    """ Adds project-specific tasks to registration
+    """
+
+    def handle_join_success(self, data):
+        """ Set fullname from firstname and lastname
+        """
+        if not data['fullname']:
+            data['fullname'] = data['firstname'] + " " + data['lastname']
+
+        super(RecensioRegistrationForm, self).handle_join_success(data)
+        
+        return
