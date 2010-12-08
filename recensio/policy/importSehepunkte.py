260fd3c8aeba95751ed4c621c56c800a3bf9db0f
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-12-08 14:26:22 +0000

    Now import creates two reviews if a review is about two books
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@7343 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/importSehepunkte.py
+++ recensio/policy/importSehepunkte.py
@@ -2,16 +2,23 @@ from lxml import etree
 
 import urllib
 
+class NoReview(Exception):
+    pass
+
 class SehepunkteParser(object):
     def parse(self, data):
         root = etree.fromstring(data)
         global_data = self._getGlobalData(root)
         for review in root:
-            review_data = self._getReviewData(review)
-            if not review_data:
+            try:
+                review_data = self._getReviewData(review)
+            except NoReview:
                 continue
             review_data.update(global_data)
-            yield review_data
+            for book in review.xpath('book'):
+                book_data = self._getBookData(book)
+                book_data.update(review_data)
+                yield book_data
 
     def _getGlobalData(self, elem):
         return {
@@ -24,16 +31,8 @@ class SehepunkteParser(object):
     def _getReviewData(self, root):
         if root.tag != 'review':
             logger.error('This XML Format seems to be unclean, it contained an unknown element after the issue tag')
-            return {}
+            raise NoReview()
         xpath_single = lambda x:''.join(root.xpath(x)).strip()
-
-        authors = []
-        for i in range(1,4):
-            authors.append({
-                'lastname'  : xpath_single('book[1]/author_%i_last_name/text()' % i)
-               ,'firstname' : xpath_single('book[1]/author_%i_first_name/text()' % i)
-            })
-        authors = filter(lambda x: x['lastname'] or x['firstname'], authors)
 
         canonical_uri= xpath_single('filename/text()')
 
@@ -41,16 +40,31 @@ class SehepunkteParser(object):
             'category' : xpath_single('category/text()')
            ,'reviewAuthorLastname' : xpath_single('reviewer/last_name/text()')
            ,'reviewAuthorFirstname' : xpath_single('reviewer/first_name/text()')
-           ,'authors' : authors
-           ,'isbn' : xpath_single('book[1]/isbn/text()')
-           ,'title' : xpath_single('book[1]/title/text()')
-           ,'subtitle' : xpath_single('book[1]/subtitle/text()')
-           ,'placeOfPublication' : xpath_single('book[1]/place_of_publication/text()')
-           ,'publisher' : xpath_single('book[1]/publishing_company/text()')
-           ,'yearOfPublication' : xpath_single('book[1]/year/text()')
-           ,'series' : xpath_single('book[1]/series/text()')
-           ,'pages' : xpath_single('book[1]/pages/text()')
            ,'canonical_uri' : canonical_uri
+        }
+    def _getBookData(self, root):
+        xpath_single = lambda x:''.join(root.xpath(x)).strip()
+
+        authors = []
+        for i in range(1,4):
+            authors.append({
+                'lastname'  : xpath_single('author_%i_last_name/text()' % i)
+               ,'firstname' : xpath_single('author_%i_first_name/text()' % i)
+            })
+        authors = filter(lambda x: x['lastname'] or x['firstname'], authors)
+
+        canonical_uri= xpath_single('filename/text()')
+
+        return {
+            'authors' : authors
+           ,'isbn' : xpath_single('isbn/text()')
+           ,'title' : xpath_single('title/text()')
+           ,'subtitle' : xpath_single('subtitle/text()')
+           ,'placeOfPublication' : xpath_single('place_of_publication/text()')
+           ,'publisher' : xpath_single('publishing_company/text()')
+           ,'yearOfPublication' : xpath_single('year/text()')
+           ,'series' : xpath_single('series/text()')
+           ,'pages' : xpath_single('pages/text()')
         }
 
 sehepunkte_parser = SehepunkteParser()
