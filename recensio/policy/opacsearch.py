29b065a6db5e3ae78cd9e60d00bfaf1f85d95c4f
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-01-03 13:24:28 +0000

    Fixing broken build. #2264. Small improvement in opac search
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@7490 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/opacsearch.py
+++ recensio/policy/opacsearch.py
@@ -65,8 +65,8 @@ def createResult(result):
             firstname = None
             lastname = author.strip()
         authors.append({'firstname' : firstname, 'lastname' : lastname})
-    title = raw_stuff.get('Titel:', '').split('|')[0]
-    subtitle = "|".join(raw_stuff.get('Titel:', '').split('|')[1:])
+    title = raw_stuff.get('Titel:', '').split('|')[0].strip()
+    subtitle = "|".join([x.strip() for x in raw_stuff.get('Titel:', '').split('|')[1:]])
     language = raw_stuff.get('Sprache:', '')
     isbn = raw_stuff.get('ISBN/ISMN:', '')
     try:
