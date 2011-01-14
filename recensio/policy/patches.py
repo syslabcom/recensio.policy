c6598334e87dfc55d66cf3dd3c2d6deb77524f92
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-01-14 11:08:07 +0000

    rels #2497 order the vocabularies according to the right language and not the canonical one
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@7650 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/patches.py
+++ recensio/policy/patches.py
@@ -97,13 +97,33 @@ def patch(old_method):
     return contentIndependentGetVocabularyDict
 
 try:
+    from Products.CMFCore.utils import getToolByName
+    from Products.ATVocabularyManager.config import SORT_METHOD_LEXICO_VALUES
+
+    def patch2(old_method):
+        def getSortedKeysCheckLang(self):
+            sortMethod = self.getSortMethod()
+            if sortMethod == SORT_METHOD_LEXICO_VALUES:
+                langtool = getToolByName(self,'portal_languages')
+                lang = langtool.getPreferredLanguage()
+                terms = self.contentValues()
+                terms.sort(lambda x,y:cmp(x.getTermValue(lang),y.getTermValue(lang)))
+                return [term.getVocabularyKey() for term in terms]
+            else:
+                return old_method(self)
+        return getSortedKeysCheckLang
+
     from Products.ATVocabularyManager.types import tree, simple
 
-    tree.vocabulary.TreeVocabulary.getVocabularyDict = \
-        patch(tree.vocabulary.TreeVocabulary.getVocabularyDict)
+    tree_voc = tree.vocabulary.TreeVocabulary
 
-    simple.vocabulary.SimpleVocabulary.getVocabularyDict = \
-        patch(simple.vocabulary.SimpleVocabulary.getVocabularyDict)
+    tree_voc.getVocabularyDict = patch(tree_voc.getVocabularyDict)
+    tree_voc.getSortedKeys = patch2(tree_voc.getSortedKeys)
+
+    simple_voc = simple.vocabulary.SimpleVocabulary
+
+    simple_voc.getVocabularyDict = patch(simple_voc.getVocabularyDict)
+    simple_voc.getSortedKeys = patch2(simple_voc.getSortedKeys)
 
 except ImportError:
     pass
