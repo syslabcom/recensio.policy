5d06a84ef114d585482422f764867efb89f67be5
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-02-14 10:07:26 +0000

    Adding functional test layer
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@7735 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/tests/layer.py
+++ recensio/policy/tests/layer.py
@@ -34,3 +34,5 @@ class RecensioPolicy(PloneSandboxLayer):
 RECENSIO_FIXTURE = RecensioPolicy()
 RECENSIO_INTEGRATION_TESTING = IntegrationTesting(bases=(RECENSIO_FIXTURE, ),
     name="RecensioPolicy:Integration")
+RECENSIO_FUNCTIONAL_TESTING = FunctionalTesting(bases=(RECENSIO_FIXTURE, ),
+    name="RecensioContenttypes:Functional")
