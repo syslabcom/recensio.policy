a072e3b8e1b0ce10c69e54bcbc1031c6df878008
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-07 15:44:52 +0000

    some modifications for the new browsing/language filter. refs #3125
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@9189 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/utility.py
+++ recensio/policy/utility.py
@@ -17,6 +17,7 @@ from Products.CMFCore.utils import getToolByName
 from AccessControl import ClassSecurityInfo
 
 browsing_facets = ['ddcPlace', 'ddcTime', 'ddcSubject']
+filter_facets = ['languageReviewedText']
 
 class Reader(object):
 
@@ -152,29 +153,33 @@ class OAIUtility(object):
         return self.known_providers.keys()
 
 
-def getSelectedQuery(selected, params=None):
+def getSelectedQuery(selected, params=None, facet_fields=browsing_facets, queryparam='fq'):
     """ construct a query that gets faceting info with the specified fields selected,
         i.e. set as fq (filter query) fields. Selected should be a dict with field
         (=attribute) names mapped to lists of values
     """
     if not params:
         params = {'facet': 'true', 
-                  'facet.field': browsing_facets }
+                  'facet.field': facet_fields }
     for field, values in selected.items():
         for name in values:
-            params.setdefault('fq', []).append('%s:"%s"' % (field, name.encode('utf-8')))
+            if name:
+                if queryparam == 'fq':
+                    params.setdefault(queryparam, []).append('%s:"%s"' % (field, name.encode('utf-8')))
+                else:
+                    params.setdefault(queryparam, []).append(name.encode('utf-8'))
     return make_query(params, doseq=True)
 
 
-def convertFacets(fields, context=None, request={}, filter=None):
+def convertFacets(fields, context=None, request={}, filter=None, facet_fields=browsing_facets, queryparam='fq'):
     """ convert facet info to a form easy to process in templates """
     info = []
     params = request.copy()   # request needs to be a dict, i.e. request.form
-    facets = browsing_facets
+    facets = facet_fields
     params['facet.field'] = facets = list(facets)
-    fq = params.get('fq', [])
+    fq = params.get(queryparam, [])
     if isinstance(fq, basestring):
-        fq = params['fq'] = [fq]
+        fq = params[queryparam] = [fq]
     selected = set([facet.split(':', 1)[0].strip('+') for facet in fq ])
     selected = selected.intersection(set(browsing_facets))
     for field, values in fields.items():
@@ -182,10 +187,10 @@ def convertFacets(fields, context=None, request={}, filter=None):
         second = lambda a, b: cmp(b[1], a[1])
         for name, count in sorted(values.items(), cmp=second):
             p = deepcopy(params)
-            #p.setdefault('fq', []).append('%s:"%s"' % (field, name.encode('utf-8')))
+            #p.setdefault(queryparam, []).append('%s:"%s"' % (field, name.encode('utf-8')))
             if filter is None or filter(name, count):
                 counts.append(dict(name=name, count=count,
-                    query=getSelectedQuery({field: [name]}, params=p)))
+                    query=getSelectedQuery({field: [name]}, params=p, facet_fields=facet_fields, queryparam=queryparam)))
         if counts:
             info.append(dict(title=field, counts=counts))
     if facets:          # sort according to given facets (if available)
