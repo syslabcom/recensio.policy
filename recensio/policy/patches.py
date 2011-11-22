70cfa829c95c619819c8754ffe5def5b1c131192
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-11-22 12:17:04 +0000

    encode pattern, fixes some UnicodeDecodeErrors from search_rss
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@9725 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/patches.py
+++ recensio/policy/patches.py
@@ -72,7 +72,7 @@ def mangleQuery(keywords, config, schema):
                     base_value = safe_unicode(quote(value.replace('*', '').replace('?', '')))
                 # simple queries use custom search pattern
                 value = pattern.format(value=safe_unicode(quote(value)),
-                    base_value=base_value)
+                    base_value=base_value).encode('utf8')
                 keywords[key] = set([value])    # add literal query parameter
                 continue
             elif simple_term:               # use prefix/wildcard search
