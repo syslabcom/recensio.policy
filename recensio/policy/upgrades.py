f9c2eab579c1cc72c3779723ccdd9031355cf05d
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-08-17 11:01:44 +0000

    Fault tolerance
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.policy/trunk@10150 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/policy/upgrades.py
+++ recensio/policy/upgrades.py
@@ -1,10 +1,26 @@
-from recensio.contenttypes.interfaces.review import IReview
 from Products.CMFCore.utils import getToolByName
+from transaction import commit
+
 from recensio.contenttypes.eventhandlers import review_pdf_updated_eventhandler
+from recensio.contenttypes.interfaces.review import IReview
 
 
 def v7to8(portal_setup):
     catalog = getToolByName(portal_setup, 'portal_catalog')
-    for i in catalog(object_provides=IReview.__identifier__):
-        obj = i.getObject()
-        review_pdf_updated_eventhandler(obj, None)
+    for count, brain in enumerate(catalog(
+        object_provides=IReview.__identifier__)):
+        try:
+            obj = brain.getObject()
+            try:
+                if hasattr(obj, 'pagePicutes') and len(obj.pagePictures):
+                    len(obj.pagePictures[0])
+                    continue
+                else:
+                    continue
+            except TypeError:
+                pass
+            review_pdf_updated_eventhandler(obj, None)
+        except AttributeError:
+            pass
+        if not count % 20:
+            commit()
