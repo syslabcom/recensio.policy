63d4939ddfea648853a1e961eb0357bd3f442568
Patrick Gerken <do3cc@patrick-gerken.de>
2013-05-03 14:47:00 +0200

    Fix names

--- recensio/policy/sparqlsearch.py
+++ recensio/policy/sparqlsearch.py
@@ -80,41 +80,41 @@ def keywords_and_ddc(obj, retval):
         g.parse(raw_data, format='application/rdf+xml')
         if [x for x in
             g.predicates(object=URIRef('http://w3.org/2004/02/skos/core#Concept'
-            ))]:
+                                       ))]:
 
             # We don't handle Concepts
 
             return
         if URIRef('http://www.w3.org/2004/02/skos/core#Concept') in \
-            [x for x in g.objects(predicate=URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'))]:
+                [x for x in g.objects(predicate=URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'))]:
             # We still don't handle Concepts
             return
 
         if URIRef('http://d-nb.info/vocab/gnd-sc#16.5p') in [x for x in
-                g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
-                ))]:
+                                                             g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
+                                                                                        ))]:
 
             # We don't handle persons as keywords
 
             return
         if URIRef('http://d-nb.info/vocab/gnd-sc#16.5') in [x for x in
-                g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
-                ))]:
+                                                            g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
+                                                                                       ))]:
 
             # OK, I really don't know whats happening right now. THis
             # example is from: http://d-nb.info/gnd/4240685-7
 
             return
         if URIRef('http://d-nb.info/vocab/gnd-sc#6.6') in [x for x in
-                g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
-                ))]:
+                                                           g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
+                                                                                      ))]:
 
             # We don't handle organizations as keywords
 
             return
         if URIRef('http://d-nb.info/vocab/gnd-sc#8.2b') in [x for x in
-                g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
-                ))]:
+                                                            g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#gndSubjectCategory'
+                                                                                       ))]:
 
             # We don't handle organizations as keywords (again?)
 
@@ -122,14 +122,14 @@ def keywords_and_ddc(obj, retval):
 
         possible_values = [x.title() for x in
                            g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#preferredNameForTheSubjectHeading'
-                           ))] + [x.title() for x in
-                                  g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#preferredNameForThePlaceOrGeographicName'
-                                  ))]
+                                                      ))] + [x.title() for x in
+                                                             g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#preferredNameForThePlaceOrGeographicName'
+                                                                                        ))]
         if possible_values:
             retval['keywords'].extend(possible_values)
         else:
-            log.error('Don\'t know how to handle this for keyword and ddc: %s'
-                      , raw_data)
+            log.error(
+                'Don\'t know how to handle this for keyword and ddc: %s', raw_data)
     else:
         if obj.datatype == 'http://purl.org/dc/terms/DDC':
             retval['ddc'] = obj.value
@@ -138,38 +138,42 @@ def keywords_and_ddc(obj, retval):
 
 
 def authorsStore(obj, retval):
-    import pdb;pdb.set_trace()
+    import pdb
+    pdb.set_trace()
     if not obj.value.startswith('http'):
         retval['authors'].append(obj.value)
     else:
         g = Graph()
         try:
             g.parse(obj.value)
-        except RDFaError, e:
+        except RDFaError as e:
             log.error('Bad answer from \'%s\': \'%s\', ignoring',
                       obj.value, e, exc_info=True)
             return
-        firstnames = [x.title() for x in
-                      g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#forename'
-                      ))]
-        surnames = [x.title() for x in
-                    g.objects(predicate=URIRef('http://d-nb.info/standards/elementset/gnd#surname'
-                    ))]
-        firstname = (firstnames[0] if firstnames else None)
-        surname = (surnames[0] if surnames else None)
-        retval['authors'].append(dict(firstname=firstname,
-                                 lastname=surname))
+        for subject in g.subjects():
+            firstnames = [x.title() for x in
+                          g.objects(subject=subject, predicate=URIRef('http://d-nb.info/standards/elementset/gnd#forename'))]
+            surnames = [x.title() for x in
+                        g.objects(subject=subject, predicate=URIRef('http://d-nb.info/standards/elementset/gnd#surname'))]
+            firstname = (firstnames[0] if firstnames else None)
+            surname = (surnames[0] if surnames else None)
+            retval['authors'].append(dict(firstname=firstname,
+                                     lastname=surname))
 
 
-HANDLERS = defaultdict(lambda : lambda a, b: None)
-HANDLERS['http://iflastandards.info/ns/isbd/elements/P1016'] = genericStore("location")
-HANDLERS['http://iflastandards.info/ns/isbd/elements/P1006'] = genericStore('subtitle')
+HANDLERS = defaultdict(lambda: lambda a, b: None)
+HANDLERS['http://iflastandards.info/ns/isbd/elements/P1016'] = genericStore(
+    "location")
+HANDLERS['http://iflastandards.info/ns/isbd/elements/P1006'] = genericStore(
+    'subtitle')
 HANDLERS['http://purl.org/dc/elements/1.1/contributor'] = authorsStore
 HANDLERS['http://purl.org/dc/elements/1.1/creator'] = authorsStore
-HANDLERS['http://purl.org/dc/elements/1.1/identifier'] = genericStore('isbn')  # XXX In the tests is an example where both are set. Both isbn setters override each other right now!
+HANDLERS['http://purl.org/dc/elements/1.1/identifier'] = genericStore(
+    'isbn')  # XXX In the tests is an example where both are set. Both isbn setters override each other right now!
 HANDLERS['http://purl.org/dc/elements/1.1/language'] = genericStore('language')
 HANDLERS['http://purl.org/dc/terms/language'] = genericStore('language')
-HANDLERS['http://purl.org/dc/elements/1.1/publisher'] = genericStore('publisher')
+HANDLERS['http://purl.org/dc/elements/1.1/publisher'] = genericStore(
+    'publisher')
 HANDLERS['http://purl.org/dc/terms/publisher'] = genericStore('publisher')
 HANDLERS['http://purl.org/dc/elements/1.1/subject'] = keywords_and_ddc
 HANDLERS['http://purl.org/dc/terms/subject'] = keywords_and_ddc
@@ -180,7 +184,8 @@ HANDLERS['http://purl.org/dc/terms/extent'] = numberStore
 HANDLERS['http://purl.org/dc/terms/issued'] = genericStore('year')
 HANDLERS['http://purl.org/ontology/bibo/editor'] = authorsStore
 HANDLERS['http://purl.org/ontology/bibo/isbn'] = genericStore('isbn')
-HANDLERS['http://rdvocab.info/Elements/placeOfPublication'] = genericStore('location')
+HANDLERS['http://rdvocab.info/Elements/placeOfPublication'] = genericStore(
+    'location')
 
 KNOWN_IGNORED = map(IRI, [  # What is a country code in the context of a publication anyway
     'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
@@ -195,7 +200,7 @@ KNOWN_IGNORED = map(IRI, [  # What is a country code in the context of a publica
     'http://www.geonames.org/ontology#countryCode',
     'http://purl.org/dc/terms/hasPart',
     'http://purl.org/dc/terms/alternative',
-    ])
+])
 
 
 def getMetadata(isbn):
@@ -211,19 +216,20 @@ def getMetadata(isbn):
         'publisher': None,
         'pages': None,
         'year': None,
-        }
+    }
 
     isbn = isbn.replace('-', '').replace(' ', '')
     service = sparql.Service('http://lod.b3kat.de/sparql')
     result = service.query(QUERY % isbn)
     for (subject, predicate, obj) in result:
         if predicate.value not in HANDLERS and predicate \
-            not in KNOWN_IGNORED:
+                not in KNOWN_IGNORED:
             log.error(u'Don\'t know how to handle %s. Contents: %s',
-                str(predicate.value),
-                obj.value)
+                      str(predicate.value),
+                      obj.value)
         if predicate in KNOWN_IGNORED:
-            log.info(u"We ignore the following information: '%s', Content: '%s'",
+            log.info(
+                u"We ignore the following information: '%s', Content: '%s'",
                 str(predicate.value),
                 obj.value)
         HANDLERS[predicate.value](obj, retval)
