e108be5a59997d36f3a2267e9bb44a7bcb43b772
Alexander Pilz <pilz@syslab.com>
2013-12-04 22:43:48 +0100

    enable memcached debugging

--- recensio/policy/caching.py
+++ recensio/policy/caching.py
@@ -15,7 +15,7 @@ def choose_cache(fun_name):
     client=getattr(thread_local, "client", None)
     if client is None:
         servers=os.environ.get("MEMCACHE_SERVER", "195.62.125.219:11211").split(",")
-        client=thread_local.client=memcache.Client(servers, debug=0)
+        client=thread_local.client=memcache.Client(servers, debug=1)
 
     return MemcacheAdapter(client)
 
