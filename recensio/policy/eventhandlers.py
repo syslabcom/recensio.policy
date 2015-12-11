3d0ca213d258b6f5cb75ae2ae129efbb47a5ff17
Manuel Reinhardt <reinhardt@syslab.com>
2015-12-11 14:32:52 +0000

    don't auto-register doi if deactivated on publication, refs #12126

--- recensio/policy/eventhandlers.py
+++ recensio/policy/eventhandlers.py
@@ -1,11 +1,15 @@
 from plone import api
 from plone.app.async.interfaces import IAsyncService
+from recensio.contenttypes.interfaces import IParentGetter
 from recensio.policy.export import register_doi_requestless
 from zope.component import getUtility
 
 
 def review_published_eventhandler(obj, evt):
     if not evt.transition.getId() == 'publish':
+        return
+    publication = IParentGetter(obj).get_parent_object_of_type('Publication')
+    if publication is None or not publication.isDoiRegistrationActive():
         return
     portal = api.portal.get()
     async = getUtility(IAsyncService)
