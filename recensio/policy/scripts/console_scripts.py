d7523fafa98ede2a931dba22274bc3c4f7619b4e
Manuel Reinhardt <reinhardt@syslab.com>
2016-04-18 11:39:44 +0000

    Migrated to slc.zopescript, refs #12795

--- recensio/policy/scripts/console_scripts.py
+++ recensio/policy/scripts/console_scripts.py
@@ -1,39 +1,11 @@
-from Testing.makerequest import makerequest
 from plone import api
 from recensio.policy.browser.export import MetadataExport
 from recensio.policy.browser.email import MailCollection
 from recensio.policy.browser.sehepunkte import Import
-from zope.component.hooks import setHooks
-from zope.component.hooks import setSite
-import Zope2
+from slc.zopescript.script import ConsoleScript
 import logging
-import sys
-import transaction
 
 log = logging.getLogger()
-
-
-class ConsoleScript(object):
-    def __call__(self, config_file, run_as):
-        Zope2.Startup.run.configure(config_file)
-        self.app = makerequest(Zope2.app())
-        setHooks()
-        self.portal = self.app.objectValues('Plone Site')[0]
-        setSite(self.portal)
-
-        log.setLevel(logging.INFO)
-        handler = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        handler.setFormatter(formatter)
-        log.addHandler(handler)
-
-        with api.env.adopt_user(username=run_as):
-            self.run()
-        transaction.commit()
-
-    def run(self):
-        raise NotImplementedError
 
 
 class MetadataExportScript(ConsoleScript):
