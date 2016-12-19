91a94a56ddf4de3482850141978009026b833e98
Manuel Reinhardt <reinhardt@syslab.com>
2016-12-19 08:14:56 +0000

    Added bvid-update view, refs #14156

---
+++ recensio/policy/browser/bvid-update.py
@@ -0,0 +1,31 @@
+import csv
+from Products.Five.browser import BrowserView
+from plone import api
+
+
+class BVIDUpdate(BrowserView):
+    """Read a CSV of UID, ISBN and BVID and update the BVID field of the
+    corresponding review object.
+    """
+
+    def __call__(self):
+        self.update_successful = None
+        if self.request.get('REQUEST_METHOD') == 'POST':
+            self.do_update()
+        return self.index()
+
+    def do_update(self):
+        self.errors = []
+        self.updated = []
+        self.update_successful = True
+        csv_file = self.request.get('csv_file')
+        csvreader = csv.reader(csv_file)
+        for uid, _, bvid in csvreader:
+            obj = api.content.get(UID=uid)
+            if obj is None:
+                self.update_successful = False
+                self.errors.append('Konnte Objekt nicht finden: ' + uid)
+                continue
+            if obj.getBv() != bvid:
+                obj.setBv(bvid)
+                self.updated.append(uid)
