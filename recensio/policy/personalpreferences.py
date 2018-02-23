c33545ef8240b3febad18ff1b5f952cfcbc59d63
Manuel Reinhardt <reinhardt@syslab.com>
2018-02-23 13:25:28 +0000

    Also hide captcha etc. from user-information. Refs #16160

--- recensio/policy/personalpreferences.py
+++ recensio/policy/personalpreferences.py
@@ -1,6 +1,7 @@
 from zope.component import getUtility
 from zope.formlib import form
 from plone.app.users.browser.personalpreferences import UserDataPanel
+from plone.app.users.browser.personalpreferences import UserDataConfiglet
 from plone.app.users.userdataschema import IUserDataSchemaProvider
 from Products.CMFDefault.formlib.widgets import FileUploadWidget
 from userdataschema import UNWANTED_FIELDS_FOR_PERSONAL_PREFERENCES
@@ -21,3 +22,9 @@ class RecensioUserDataPanel(UserDataPanel):
         schema = util.getSchema()
         fields = form.FormFields(schema)
         self.form_fields = fields.omit(*UNWANTED_FIELDS_FOR_PERSONAL_PREFERENCES)
+
+
+class RecensioUserDataConfiglet(UserDataConfiglet, RecensioUserDataPanel):
+        """ We override Plone's default, in order to filter out the unwanted fields
+            for the personal preferences form.
+        """
