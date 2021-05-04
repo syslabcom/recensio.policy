9b22d1ddf78744c2f117c59c19797a004aa0275d
Manuel Reinhardt <reinhardt@syslab.com>
2021-05-04 10:54:01 +0200

    Remove secondary nav portlet assignment from “Rezensionen”. Refs SCR-1050

--- recensio/policy/upgrades/20210427220944_scr_1050/upgrade.py
+++ recensio/policy/upgrades/20210427220944_scr_1050/upgrade.py
@@ -1,4 +1,7 @@
 from ftw.upgrade import UpgradeStep
+from plone import api
+from plone.app.portlets.utils import assignment_mapping_from_key
+from plone.portlets.constants import CONTEXT_CATEGORY
 
 
 class Scr1050(UpgradeStep):
@@ -7,3 +10,13 @@ class Scr1050(UpgradeStep):
 
     def __call__(self):
         self.install_upgrade_profile()
+        portal = api.portal.get()
+        obj = portal.get("rezensionen")
+        if obj:
+            path = "/".join(obj.getPhysicalPath())
+            left_portlet_assignment_mapping = assignment_mapping_from_key(
+                obj, "plone.leftcolumn", CONTEXT_CATEGORY, path
+            )
+            for key in ["secondarynavportlets", "secondarynavportlet"]:
+                if key in left_portlet_assignment_mapping:
+                    del left_portlet_assignment_mapping[key]
